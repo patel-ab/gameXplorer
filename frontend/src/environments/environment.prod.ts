0bc6618b5a461769fdb085d518e5000a34cae0a3
Abhishek Patel <abhishekvpatel20@gmail.com>
2024-12-21 14:02:07 -0500

    Uploaded env files

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  apiKey: 'YOUR_API_KEY',
+  baseUrl: 'https://api.rawg.io/api/games',
+
+  Steam: 'https://store.steampowered.com/',
+  PlayStationStore: 'https://store.playstation.com/',
+  XboxStore: 'https://www.xbox.com/en-US/microsoft-store',
+  AppStore: 'https://www.apple.com/app-store/',
+  GOG: 'https://www.gog.com/',
+  NintendoStore: 'https://www.nintendo.com/store/',
+  Xbox360Store: 'https://en.wikipedia.org/wiki/Xbox_Games_Store',
+  GooglePlay: 'https://play.google.com/',
+  itchio: 'https://itch.io/',
+  EpicGames: 'https://store.epicgames.com/',
+};
